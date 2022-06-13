b4ea48675a56821f50297163b8f6b3d29cf36eca
Max Schopf <max.schopf@mailfence.com>
2022-06-13 20:37:36 +0200

    Init

---
+++ CMakeFiles/Code.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/max/Documents/Git/OOP-Aufgabe 8/Movie.cpp" "CMakeFiles/Code.dir/Movie.cpp.o" "gcc" "CMakeFiles/Code.dir/Movie.cpp.o.d"
+  "/home/max/Documents/Git/OOP-Aufgabe 8/main.cpp" "CMakeFiles/Code.dir/main.cpp.o" "gcc" "CMakeFiles/Code.dir/main.cpp.o.d"
+  "/home/max/Documents/Git/OOP-Aufgabe 8/personen.cpp" "CMakeFiles/Code.dir/personen.cpp.o" "gcc" "CMakeFiles/Code.dir/personen.cpp.o.d"
+  "/home/max/Documents/Git/OOP-Aufgabe 8/test.cpp" "CMakeFiles/Code.dir/test.cpp.o" "gcc" "CMakeFiles/Code.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/Movie.cpp.o"
+  "CMakeFiles/Code.dir/Movie.cpp.o.d"
+  "CMakeFiles/Code.dir/main.cpp.o"
+  "CMakeFiles/Code.dir/main.cpp.o.d"
+  "CMakeFiles/Code.dir/personen.cpp.o"
+  "CMakeFiles/Code.dir/personen.cpp.o.d"
+  "CMakeFiles/Code.dir/test.cpp.o"
+  "CMakeFiles/Code.dir/test.cpp.o.d"
+  "Code"
+  "Code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
