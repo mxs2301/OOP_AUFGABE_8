70054b3d8ef2ef431be482bc930979e0da4265dc
Max Schopf <max.schopf@mailfence.com>
2022-06-14 10:54:13 +0200

    Neue Dateien für Teilaufgabe 2 a& b

---
+++ zahlen.cpp
@@ -0,0 +1,6 @@
+#include "zahlen.h"
+
+std::vector<int> sum(const std::vector<int> &nums, int n){
+
+	return nums;
+}
---
+++ zahlen.h
@@ -0,0 +1,8 @@
+#ifndef _ZAHLEN_H_
+#define _ZAHLEN_H_
+
+#include <vector>
+
+std::vector<int> sum(const std::vector<int>& nums, int n);
+
+#endif
